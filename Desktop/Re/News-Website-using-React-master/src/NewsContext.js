ad4e521919df0f12a1853bb4fe13b25275e9345d
Bruno Paulette <brunopaulettecurbelo@gmail.com>
2021-06-28 19:17:46 -0300

    random bullshit

---
+++ Desktop/Re/News-Website-using-React-master/src/NewsContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const NewsContext = createContext();
+
+export const NewsContextProvider = (props) => {
+  const [data, setData] = useState();
+  const apiKey = "6633de5b30b74366b611995131df0058";
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://apinewsv2-heroku.herokuapp.com/newsapi`
+      )
+      .then((response) => setData(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <NewsContext.Provider value={{ data }}>
+      {props.children}
+    </NewsContext.Provider>
+  );
+};
